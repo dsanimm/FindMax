ae13e0aec2432af4950c8caf8a044cc8b9f453ad
dsanimm <dsanimm@gmail.com>
2020-10-08 07:31:12 +0530

    modified to take multiple parameters

--- src/main/java/com/capgemini/MaxFinder.java
+++ src/main/java/com/capgemini/MaxFinder.java
@@ -1,13 +1,11 @@
 package com.capgemini;
+import java.util.Arrays;
 
-public class MaxFinder <E extends Comparable>{
-	E value1;
-	E value2;
-	E value3;
-	MaxFinder(E value1,E value2,E value3){
-		this.value1=value1;
-		this.value2=value2;
-		this.value3=value3;
+public class MaxFinder <E extends Comparable> {
+	E[] arr;
+	
+	MaxFinder(E[] arr){
+		this.arr = arr;
 	}
 	public MaxFinder() {
 		// TODO Auto-generated constructor stub
@@ -17,13 +15,9 @@ public class MaxFinder <E extends Comparable>{
 	}
 
 	
-	public <E extends Comparable> E findMax(MaxFinder maxfinder){
-		E max = (E) maxfinder.value1;
-		if ( maxfinder.value2.compareTo(max) > 0)
-			max = (E) maxfinder.value2;
-		if ( maxfinder.value3.compareTo(max) > 0)
-			max = (E) maxfinder.value3;
-		return max;
+	public <E extends Comparable> E findMax(MaxFinder maxfinder) {
+		 Arrays.sort(maxfinder.arr);
+		return (E) maxfinder.arr[maxfinder.arr.length-1];
 	}
-	
+
 }
--- src/test/java/com/capgemini/MaxFinderTest.java
+++ src/test/java/com/capgemini/MaxFinderTest.java
@@ -16,47 +16,19 @@ public class MaxFinderTest {
 
 	@Test
 	public void true_if_maxint_at_first() {
-		assertEquals(9, maxfinder.findMax(new MaxFinder(9, 2, 4)));
+		Integer[] array={2,5,6,8,9};
+		assertEquals(9, maxfinder.findMax(new MaxFinder(array)));
 	}
 
 	@Test
-	public void true_if_maxint_at_second() {
-		assertEquals(20, maxfinder.findMax(new MaxFinder(9, 20, 4)));
+	public void true_if_maxfloat() {
+		Float[] array = { 2.0F, 5.0F, 6.0F, 18.0F, 90.0F };
+		assertEquals(90.0F, maxfinder.findMax(new MaxFinder(array)));
 	}
 
 	@Test
-	public void true_if_maxint_at_third() {
-		assertEquals(30, maxfinder.findMax(new MaxFinder(9, 2, 30)));
+	public void true_if_maxString() {
+		String[] array = { "Zebra", "Apple", "Orange", "Mango", "Gauva", "Zebra" };
+		assertEquals("Zebra", maxfinder.findMax(new MaxFinder(array)));
 	}
-
-	@Test
-	public void true_if_maxfloat_at_first() {
-		assertEquals(9.0F, maxfinder.findMax(new MaxFinder(9.0F, 2.0F, 4.0F)));
-	}
-
-	@Test
-	public void true_if_maxfloat_at_second() {
-		assertEquals(20.0F, maxfinder.findMax(new MaxFinder(9.0F, 20.0F, 4.0F)));
-	}
-
-	@Test
-	public void true_if_maxfloat_at_third() {
-		assertEquals(40.0F, maxfinder.findMax(new MaxFinder(9.0F, 2.0F, 40.0F)));
-	}
-
-	@Test
-	public void true_if_maxString_at_first() {
-		assertEquals("Peach", maxfinder.findMax(new MaxFinder("Peach", "Apple", "Banana")));
-	}
-
-	@Test
-	public void true_if_maxString_at_secind() {
-		assertEquals("Peach", maxfinder.findMax(new MaxFinder("Apple", "Peach", "Banana")));
-	}
-
-	@Test
-	public void true_if_maxString_at_third() {
-		assertEquals("Peach", maxfinder.findMax(new MaxFinder("Banana", "Apple", "Peach")));
-	}
-
 }
