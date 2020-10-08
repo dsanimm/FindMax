958dac21a9503baceab24036c4f572f676e39888
dsanimm <dsanimm@gmail.com>
2020-10-08 06:07:22 +0530

    added find max of three float

--- src/test/java/com/capgemini/MaxFinderTest.java
+++ src/test/java/com/capgemini/MaxFinderTest.java
@@ -17,17 +17,31 @@ public class MaxFinderTest {
 	}
 
 	@Test
-	public void test1_1() {
+	public void true_if_maxint_at_first() {
 		assertEquals((Integer) 9, (Integer) maxfinder.findMaxInteger(9, 2, 4));
 	}
 
 	@Test
-	public void test1_2() {
+	public void true_if_maxint_at_second() {
 		assertEquals((Integer) 20, (Integer) maxfinder.findMaxInteger(9, 20, 4));
 	}
 
 	@Test
-	public void test1_3() {
+	public void true_if_maxint_at_third() {
 		assertEquals((Integer) 30, (Integer) maxfinder.findMaxInteger(9, 2, 30));
+	}
+	@Test
+	public void true_if_maxfloat_at_first() {
+		assertEquals((Float) 9.0F, (Float) maxfinder.findMaxFloat(9.0F, 2.0F, 4.0F));
+	}
+
+	@Test
+	public void true_if_maxfloat_at_second() {
+		assertEquals((Float) 20.0F, (Float) maxfinder.findMaxFloat(9.0F, 20.0F, 4.0F));
+	}
+
+	@Test
+	public void true_if_maxfloat_at_third() {
+		assertEquals((Float) 30.0F, (Float) maxfinder.findMaxFloat(9.0F, 2.0F, 30.0F));
 	}
 }
