dc2a3efbdc49c0618f60925176cf15fd8d3b46dc
dsanimm <dsanimm@gmail.com>
2020-10-08 06:21:27 +0530

    added find max of three generics

--- src/main/java/com/capgemini/MaxFinder.java
+++ src/main/java/com/capgemini/MaxFinder.java
@@ -30,5 +30,13 @@ public class MaxFinder {
 			max = c;
 		return max;
 	}
+	public <E extends Comparable> E findMax(E a, E b,E c){
+		E max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		return max;
+	}
 	
 }
--- src/test/java/com/capgemini/MaxFinderTest.java
+++ src/test/java/com/capgemini/MaxFinderTest.java
@@ -18,43 +18,43 @@ public class MaxFinderTest {
 
 	@Test
 	public void true_if_maxint_at_first() {
-		assertEquals((Integer) 9, (Integer) maxfinder.findMaxInteger(9, 2, 4));
+		assertEquals((Integer) 9, (Integer) maxfinder.findMax(9, 2, 4));
 	}
 
 	@Test
 	public void true_if_maxint_at_second() {
-		assertEquals((Integer) 20, (Integer) maxfinder.findMaxInteger(9, 20, 4));
+		assertEquals((Integer) 20, (Integer) maxfinder.findMax(9, 20, 4));
 	}
 
 	@Test
 	public void true_if_maxint_at_third() {
-		assertEquals((Integer) 30, (Integer) maxfinder.findMaxInteger(9, 2, 30));
+		assertEquals((Integer) 30, (Integer) maxfinder.findMax(9, 2, 30));
 	}
 	@Test
 	public void true_if_maxfloat_at_first() {
-		assertEquals((Float) 9.0F, (Float) maxfinder.findMaxFloat(9.0F, 2.0F, 4.0F));
+		assertEquals((Float) 9.0F, (Float) maxfinder.findMax(9.0F, 2.0F, 4.0F));
 	}
 
 	@Test
 	public void true_if_maxfloat_at_second() {
-		assertEquals((Float) 20.0F, (Float) maxfinder.findMaxFloat(9.0F, 20.0F, 4.0F));
+		assertEquals((Float) 20.0F, (Float) maxfinder.findMax(9.0F, 20.0F, 4.0F));
 	}
 
 	@Test
 	public void true_if_maxfloat_at_third() {
-		assertEquals((Float) 30.0F, (Float) maxfinder.findMaxFloat(9.0F, 2.0F, 30.0F));
+		assertEquals((Float) 30.0F, (Float) maxfinder.findMax(9.0F, 2.0F, 30.0F));
 	}
 	@Test
 	public void true_if_maxString_at_first() {
-		assertEquals("Peach",maxfinder.findMaxString("Peach","Apple","Banana"));
+		assertEquals("Peach",maxfinder.findMax("Peach","Apple","Banana"));
 	}
 	@Test
 	public void true_if_maxString_at_secind() {
-		assertEquals("Peach",maxfinder.findMaxString("Apple","Peach","Banana"));
+		assertEquals("Peach",maxfinder.findMax("Apple","Peach","Banana"));
 	}
 	@Test
 	public void true_if_maxString_at_third() {
-		assertEquals("Peach",maxfinder.findMaxString( "Banana", "Apple","Peach"));
+		assertEquals("Peach",maxfinder.findMax( "Banana", "Apple","Peach"));
 	}
 
 }
